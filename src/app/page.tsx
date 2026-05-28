03d96b7c801e6498ad59058c6ff3573f3ed04b68
Dave <davejsousa@gmail.com>
2026-05-28 14:45:44 -0400

    Nudge small desktop hero tunnel larger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -305,7 +305,7 @@ export default function Home() {
           <div className="hidden h-[375px] w-full items-center justify-end lg:flex xl:h-[500px]">
             <div className="relative h-[680px] w-[1020px] origin-right scale-50 xl:h-[500px] xl:w-[750px] xl:scale-75 2xl:scale-100">
               <RetroTunnelScene
-                className="h-full w-full [--tunnel-canvas-scale-x:2.7] [--tunnel-canvas-scale-y:1.8] 2xl:[--tunnel-canvas-scale-x:1] 2xl:[--tunnel-canvas-scale-y:1]"
+                className="h-full w-full [--tunnel-canvas-scale-x:3.15] [--tunnel-canvas-scale-y:2.1] 2xl:[--tunnel-canvas-scale-x:1] 2xl:[--tunnel-canvas-scale-y:1]"
               >
                 {/* Floating Frames - Matching Master (Rounded, Light Blue Borders) */}
                 <div className="absolute top-0 right-10 z-20 transition-transform duration-75 [transform:translate3d(var(--tunnel-parallax-a-x,0px),var(--tunnel-parallax-a-y,0px),0)]">
