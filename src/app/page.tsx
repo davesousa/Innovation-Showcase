1c8e68df8a5ce41c429fdfa2760621f5fc9c01b5
Dave <davejsousa@gmail.com>
2026-05-28 15:01:07 -0400

    Reset medium desktop hero tunnel scale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -305,7 +305,7 @@ export default function Home() {
           <div className="hidden h-[375px] w-full items-center justify-end lg:flex xl:h-[500px]">
             <div className="relative h-[680px] w-[1020px] origin-right scale-50 xl:h-[500px] xl:w-[750px] xl:scale-75 2xl:scale-100">
               <RetroTunnelScene
-                className="h-full w-full [--tunnel-canvas-scale-x:3.85] [--tunnel-canvas-scale-y:2.1] [--tunnel-canvas-offset-y:28px] 2xl:[--tunnel-canvas-scale-x:1] 2xl:[--tunnel-canvas-scale-y:1] 2xl:[--tunnel-canvas-offset-y:0px]"
+                className="h-full w-full [--tunnel-canvas-scale-x:3.85] [--tunnel-canvas-scale-y:2.1] [--tunnel-canvas-offset-y:28px] xl:[--tunnel-canvas-scale-x:1] xl:[--tunnel-canvas-scale-y:1] xl:[--tunnel-canvas-offset-y:0px]"
               >
                 {/* Floating Frames - Matching Master (Rounded, Light Blue Borders) */}
                 <div className="absolute -top-8 right-10 z-20 transition-transform duration-75 2xl:top-0 [transform:translate3d(var(--tunnel-parallax-a-x,0px),var(--tunnel-parallax-a-y,0px),0)]">
